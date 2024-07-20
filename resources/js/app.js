cd531c962f18f36c064e7df398530f08aedc263d
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-07-21 01:31:11 +0800

    app js config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/ExampleComponent.vue';
+import ProfileComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/ProfileComponent.vue';
+const app = createApp({});
+
+app.component('example-component', ExampleComponent);
+app.component('profile-component', ProfileComponent);
+app.mount('#app');
