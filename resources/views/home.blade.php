a40aa33ef6c446d149bbbc0414f8b05d3a1548cf
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-07-21 01:31:17 +0800

    home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
-@extends('layouts.app') {{-- Make sure you extend the layout you have --}}
+@extends('layouts.app')
 
 @section('content')
+
+<div class="container">
+    <div id="app">
+        <div class="profile">
+            <profile-component></profile-component>
+        </div>
+    </div>
+</div>
+
+
 
 @endsection
