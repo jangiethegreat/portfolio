e8a425224e29820d5d96662463d4e7a92c7e8eda
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-07-21 01:30:56 +0800

    vue config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            'vue': path.resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js'), // Alias Vue to the full build
+        },
+    },
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
