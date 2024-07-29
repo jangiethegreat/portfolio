2e75beef919e195ffc36ef9c75e7456d9c6c3ec0
John Cris Fernando <fernandojohncris@gmail.com>
2024-07-29 15:46:09 +0800

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { createApp } from 'vue';
-import ExampleComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/ExampleComponent.vue';
-import ProfileComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/ProfileComponent.vue';
-import SkillsComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/SkillsComponent.vue';
+import ExampleComponent from '/themes/vue/js/components/ExampleComponent.vue';
+import ProfileComponent from '/themes/vue/js/components/ProfileComponent.vue';
+import SkillsComponent from '/themes/vue/js/components/SkillsComponent.vue';
 
 
 
