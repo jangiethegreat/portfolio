04b1a150f067f3b285b199cb1733c488386635a5
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-07-29 14:18:38 +0800

    app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,15 @@
 import './bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { createApp } from 'vue';
 import ExampleComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/ExampleComponent.vue';
 import ProfileComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/ProfileComponent.vue';
+import SkillsComponent from 'C:/Users/Jang/portfolio/themes/vue/js/components/SkillsComponent.vue';
+
+
+
 const app = createApp({});
 
 app.component('example-component', ExampleComponent);
 app.component('profile-component', ProfileComponent);
+app.component('skills-component', SkillsComponent);
 app.mount('#app');
