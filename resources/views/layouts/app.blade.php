f87cc75227bf74086766304b4353173a4031498c
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-07-29 14:18:50 +0800

    app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
     <meta http:equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Responsive Side Navbar</title>
+
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css">
 
 </head>
 
@@ -14,7 +16,7 @@
 <body>
     <div class="container">
 
-        <div class="navigation">
+        <div class="navigation active">
             <ul>
                 <li>
                     <a href="#">
@@ -103,23 +105,33 @@
 
     </div>
 
-    <div class="main">
-        <div class="topbar">
+    <div class="main active">
+        <div class="topbar d-flex justify-content-between align-items-center">
             <div class="toggle">
                 <ion-icon name="menu-outline"></ion-icon>
             </div>
+            <div>
+                <input type="checkbox" id="toggle">
+
+            </div>
         </div>
         @yield('content')
+    </div>
 
     </div>
 
 
 
     <!-- ionicons -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
+    <!-- ionicons -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
     <script src="{{ asset('js/main.js') }}"></script>
+    <script src="{{ asset('js/darkmode.js') }}"></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </body>
