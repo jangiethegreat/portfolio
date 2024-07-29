5565368107d040b1b7da125fd87ae629300e5a22
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2024-07-29 14:18:44 +0800

    home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,9 @@
         <div class="profile">
             <profile-component></profile-component>
         </div>
+        <div class="skills">
+            <skills-component></skills-component>
+        </div>
     </div>
 </div>
 
